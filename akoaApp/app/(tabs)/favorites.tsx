b7d880e3cf7ca6dfb880472fb5e227597bcd7971
amougou akoa <amougouakoamike7@gmail.com>
2026-09-06 11:54:39 +0530

    chore: update dependencies for expo and related packages to version 57.x

--- akoaApp/app/(tabs)/favorites.tsx
+++ akoaApp/app/(tabs)/favorites.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react'
+import { useWishlist } from '@/context/WishListContext'
+import { useRouter } from 'expo-router'
+import Header from '@/components/Header'
+import ProductCard from '@/components/ProductCard';
 
 const Favorites = () => {
-  return (
-    <View>
-      <Text>Favorites</Text>
-    </View>
+
+const { wishlist } = useWishlist()
+const router = useRouter()
+
+  return ( 
+  <SafeAreaView className='flex-1 bg-surface' edges={['top']}>
+    <Header title='Wishlist' showMenu showCart/>
+
+    {wishlist.length > 0 ? (
+      <ScrollView className='flex-1 px-4 mt-4'
+      showsVerticalScrollIndicator={false}>
+       <View className='flex-row flex-wrap justify-between'>
+        {wishlist.map((product)=>(
+          <ProductCard key={product._id} product={product}/>
+        ))}
+
+       </View>
+      </ScrollView>
+    ) : (
+      <View className='flex-1 items-center justify-center'>
+      <Text className='text-secondary text-lg'>Your wishlist is Empty!</Text>
+      <TouchableOpacity onPress={() =>router.push('/')} className='mt-4'>
+      <Text className='text-primary font-bold'>Start Shopping</Text>
+    </TouchableOpacity>
+      </View>
+    )}
+  </SafeAreaView>
   )
 }
 
